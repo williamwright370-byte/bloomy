e3150f0b152f72635ad69d459310326b3b5fde2d
williamwright370-byte <william.wright370@education.nsw.gov.au>
2026-09-17 14:17:26 +1000

    Delete bloomy-mod-workspace/bloomy-mod directory

--- bloomy-mod-workspace/bloomy-mod/src/main/java/net/example/bloomy/BloomyClient.java
+++
@@ -1,7 +0,0 @@
-package net.example.bloomy;
-import net.fabricmc.api.ClientModInitializer;
-
-public class BloomyClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
--- bloomy-mod-workspace/bloomy-mod/src/main/java/net/example/bloomy/config/ModConfiguration.java
+++
@@ -1,8 +0,0 @@
-package net.example.bloomy.config;
-
-public class ModConfiguration {
-    public static boolean enableBiomeLava = true;
-    public static boolean enableFallingLeaves = true;
-    public static boolean enableBloom = true;
-    public static boolean enableWaterOutline = true;
-}
